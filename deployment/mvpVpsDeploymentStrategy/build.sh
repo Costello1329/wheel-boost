ff58eb1f08cb5e964888c49286ec7146c1e6c25f
Konstantin Leladze <leladze.kg@phystech.edu>
2020-03-17 02:12:09 +0300

    new files

--- deployment/mvpVpsDeploymentStrategy/build.sh
+++ deployment/mvpVpsDeploymentStrategy/build.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
 
-cd ../../WheelBoostBackend || return
+cd ../../WheelBoostBackend || printf "Error! No ../../WheelBoostBackend folder found." ; return
 python3 -m venv venv
 pip install -r req.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 
-read -p "Bot token: " token
-read -p "Bot url: " bot_url
-read -p "Bot port: " bot_port
-read -e -p "Backend url: " -i "localhost" backend_port
-read -p "Backend port: " backend_port
-read -e -p "Maps url: " -i "https://yandex.ru/maps?" maps_url
+read -r -p "Bot token: " token
+read -r -p "Bot url: " bot_url
+read -r -p "Bot port: " bot_port
+read -r -e -p "Backend url: " -i "localhost" backend_port
+read -r -p "Backend port: " backend_port
+read -r -e -p "Maps url: " -i "https://yandex.ru/maps?" maps_url
 echo "export BOT_URL=${bot_url}" > run.sh
 echo "export TOKEN=${token}" >> run.sh
 echo "export EVENTS_URL=${backend_url}" >> run.sh
 echo "export MAP_URL=${maps_url}" >> run.sh
 
-cd ../../WheelBoostBot || return
+cd ../../WheelBoostBot || printf "Error! No ../../WheelBoostBot folder found." ; return
 python3 -m venv venv
 pip install -r req.txt
 python3 manage.py makemigrations
