5891fbd8298d394c0f672cb3a2eae41fb2d35298
Konstantin Leladze <leladze.kg@phystech.edu>
2020-03-17 00:25:34 +0300

    mvpDeploymentStrategy

---
+++ deployment/mvpVpsDeploymentStrategy/build.sh
@@ -0,0 +1,11 @@
+cd ../../WheelBoostBackend || return
+python3 -m venv venv
+pip install -r req.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
+
+cd ../../WheelBoostBot || return
+python3 -m venv venv
+pip install -r req.txt
+python3 manage.py makemigrations
+python3 manage.py migrate
