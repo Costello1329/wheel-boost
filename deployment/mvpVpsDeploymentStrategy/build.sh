a7f6337d5a4227e0e619f67ed07e34088b89c740
Konstantin Leladze <leladze.kg@phystech.edu>
2020-03-18 17:43:56 +0300

    forever runserver

--- deployment/mvpVpsDeploymentStrategy/build.sh
+++ deployment/mvpVpsDeploymentStrategy/build.sh
@@ -14,8 +14,14 @@ export TOKEN=${token}
 export EVENTS_URL=localhost:${backend_port}
 export MAP_URL=https://yandex.ru/maps?
 
+[ -e ../deployment/mvpVpsDeploymentStrategy/run.sh ] && rm ../deployment/mvpVpsDeploymentStrategy/run.sh
+echo "nohup ../../WheelBoostBackend/manage.py runserver ${backend_port} &" >> ../deployment/mvpVpsDeploymentStrategy/run.sh
+echo "nohup ../../WheelBoostBot/manage.py runserver ${bot_port} &" >> ../deployment/mvpVpsDeploymentStrategy/run.sh
+
+[ -e ../deployment/mvpVpsDeploymentStrategy/monitor.sh ] && rm ../deployment/mvpVpsDeploymentStrategy/monitor.sh
+echo "lsof -i :${backend_port}" >> ../deployment/mvpVpsDeploymentStrategy/monitor.sh
+echo "lsof -i :${bot_port}" >> ../deployment/mvpVpsDeploymentStrategy/monitor.sh
+
 cd ../../WheelBoostBot || (printf "Error! No ../../WheelBoostBot folder found."; exit)
 python3 -m venv venv
 pip install -r req.txt
-python3 manage.py makemigrations
-python3 manage.py migrate
