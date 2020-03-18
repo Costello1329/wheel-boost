78550c261684f7c9fccc8e7b003e4790c8752b6f
Konstantin Leladze <leladze.kg@phystech.edu>
2020-03-18 17:16:25 +0300

    some fixes

--- deployment/mvpVpsDeploymentStrategy/build.sh
+++ deployment/mvpVpsDeploymentStrategy/build.sh
@@ -7,15 +7,12 @@ python3 manage.py makemigrations
 python3 manage.py migrate
 
 read -r -p "Bot token: " token
-read -r -p "Bot url: " bot_url
 read -r -p "Bot port: " bot_port
-read -r -e -p "Backend url: " -i "localhost" backend_port
 read -r -p "Backend port: " backend_port
-read -r -e -p "Maps url: " -i "https://yandex.ru/maps?" maps_url
-echo "export BOT_URL=${bot_url}" > run.sh
-echo "export TOKEN=${token}" >> run.sh
-echo "export EVENTS_URL=${backend_url}" >> run.sh
-echo "export MAP_URL=${maps_url}" >> run.sh
+export BOT_URL=localhost:${bot_port}
+export TOKEN=${token}
+export EVENTS_URL=localhost:${backend_port}
+export MAP_URL=https://yandex.ru/maps?
 
 cd ../../WheelBoostBot || (printf "Error! No ../../WheelBoostBot folder found."; exit)
 python3 -m venv venv
