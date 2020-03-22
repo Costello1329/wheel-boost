ca58295b21a713c51a593a5b8e67b0e97520c242
Costello1329 <leladzek2000@mail.ru>
2020-03-22 11:51:05 +0000

    pip -> pip3

--- deployment/mvpVpsDeploymentStrategy/build.sh
+++ deployment/mvpVpsDeploymentStrategy/build.sh
@@ -2,7 +2,7 @@
 
 cd ../../WheelBoostBackend || (printf "Error! No ../../WheelBoostBackend folder found."; exit)
 python3 -m venv venv
-pip install -r req.txt
+pip3 install -r req.txt
 python3 manage.py makemigrations
 python3 manage.py migrate
 
@@ -24,4 +24,5 @@ echo "lsof -i :${bot_port}" >> ../deployment/mvpVpsDeploymentStrategy/monitor.sh
 
 cd ../../WheelBoostBot || (printf "Error! No ../../WheelBoostBot folder found."; exit)
 python3 -m venv venv
-pip install -r req.txt
+pip3 install -r req.txt
+
